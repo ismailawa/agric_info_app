f22e3016cbdb979f95ad177b6ba1039c9565ac06
ISMAILAWA <ismailawa.aliyu@gmail.com>
2019-09-10 20:50:58 +0100

    add tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:custom_chewie/custom_chewie.dart';
-import 'package:video_player/video_player.dart';
 
 void main() => runApp(AgricultureInfo());
 
@@ -20,30 +18,37 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   TabController _controller;
+  Color _primaryColor;
 
   @override
   void initState() {
     super.initState();
     _controller = TabController(length: 4, vsync: this, initialIndex: 0);
+    _primaryColor= Colors.green;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: _primaryColor,
+          title: Text("Home"),
           bottom: TabBar(
+              onTap: _onTap,
+              indicatorColor: Colors.white,
+              indicatorWeight: 6,
               controller: _controller, tabs: [
             Tab(
-              child: Text("Home"),
+              child: Container(child: Text('Home', style: TextStyle(color: Colors.white, fontSize: 18),),),
             ),
             Tab(
-              child: Text("Favourite"),
+              child: Container(child: Text('Home', style: TextStyle(color: Colors.white, fontSize: 18),),),
             ),
             Tab(
-              child: Text("Popular"),
+              child: Container(child: Text('Home', style: TextStyle(color: Colors.white, fontSize: 18),),),
             ),
             Tab(
-              child: Text("Recommended"),
+              child: Container(child: Text('Home', style: TextStyle(color: Colors.white, fontSize: 18),),),
             )
           ]),
         ),
@@ -57,4 +62,29 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         ])
     );
   }
+
+  void _onTap(int index){
+    switch(index){
+      case 0:
+        setState(() {
+          _primaryColor = Colors.red;
+        });
+        break;
+      case 1:
+        setState(() {
+          _primaryColor = Colors.amber;
+        });
+        break;
+      case 2:
+        setState(() {
+          _primaryColor = Colors.deepOrange;
+        });
+        break;
+      case 3:
+        setState(() {
+          _primaryColor = Colors.pink;
+        });
+    }
+  }
 }
+
